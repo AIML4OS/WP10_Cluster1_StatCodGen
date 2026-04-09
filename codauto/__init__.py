f1b8830337c63c16567378cf58bfb2ca578d538f
U853768 <andres.jurado.prieto@ine.es>
2026-04-09 14:31:44 +0200

    Deleted old version fro prompt generation

--- codauto/__init__.py
+++ codauto/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from .utils import *
 from .codifier import *
-from .prompt_maker import *
-from .prompt_text import *
 from .structured import *
 from .codifier_fasttext import *
